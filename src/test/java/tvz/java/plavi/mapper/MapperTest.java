f655a0f3baa460a9809e4640bbc9c497492dcb6d
ivansaban <isaban8@gmail.com>
2017-06-03 00:56:25 +0200

    unit test

---
+++ src/test/java/tvz/java/plavi/mapper/MapperTest.java
@@ -0,0 +1,39 @@
+package tvz.java.plavi.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tvz.java.plavi.model.dto.LoggedUser;
+import tvz.java.plavi.model.entity.Role;
+import tvz.java.plavi.model.entity.User;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by NIS on 3.6.2017..
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapperTest {
+
+    @Test
+    public void UserMapperTest() {
+        Role role = new Role();
+        role.setName("TEST");
+
+        User user = new User();
+        user.setFirstname("Ante");
+        user.setLastname("Antić");
+        user.setUsername("test");
+        user.setPassword("pass");
+        user.setRole(role);
+
+        LoggedUser loggedUser = UserMapper.mapUser(user);
+
+        assertEquals("Ante", loggedUser.getFirstname());
+        assertEquals("Antić", loggedUser.getLastname());
+        assertEquals("test", loggedUser.getUsername());
+        assertEquals("TEST", loggedUser.getRole().getName());
+    }
+}
